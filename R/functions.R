398281b360e1f037903a2fef1ac9d3f13d583a25
Ida Kær Thorsen <ida.kaer.thorsen@regionh.dk>
2025-12-03 10:08:17 +0100

    make a function for creating table of descriptive stats

---
+++ R/functions.R
@@ -0,0 +1,24 @@
+#' Create table of descriptive statistics
+#'
+#' @param data A data frame
+#'
+#' @returns A data.frame/tibble.
+create_table_descriptive_stats <- function(data) {
+  data |>
+    dplyr::group_by(metabolite) |>
+    dplyr::summarise(dplyr::across(
+      value,
+      base::list(mean = mean, sd = sd)
+    )) |>
+    dplyr::mutate(dplyr::across(
+      tidyselect::where(is.numeric),
+      \(x) base::round(x,
+        digits = 1
+      )
+    )) |>
+    dplyr::mutate(MeanSD = glue::glue("{value_mean} ({value_sd})")) |>
+    dplyr::select(
+      Metabolite = metabolite,
+      "Mean SD" = MeanSD
+    )
+}
